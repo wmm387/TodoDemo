41b01e08d9a4b532a09f347bc01a164701dbd9ce
wmm387 <wangyuanwmm@163.com>
2017-05-09 18:14:47 +0800

    Initial commit

---
+++ app/src/main/java/com/wangyuanwmm/tododemo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.wangyuanwmm.tododemo;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddTodoActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
